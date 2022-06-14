bbddb5bd14edc8dcba827da058ec9bb3cf7769db
mpj144 <marcelopjrdev@gmail.com>
2022-06-14 12:55:04 -0300

    Update title page and config url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const APILOCAL = "http://localhost:8080/rps-game";
-const APISERVER = "https://rps-game-serverside.herokuapp.com/rps-game";
+const APISERVER = "https://rpsgame-serverside.herokuapp.com/rps-game";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
